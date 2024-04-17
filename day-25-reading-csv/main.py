d3d3be375bb4aad70eae7b53bf69b892b2f4f819
Dzmitry <slizhd@amazon.com>
2024-04-17 00:06:23 -0700

    pandas csv to process to csv

--- day-25-reading-csv/main.py
+++ day-25-reading-csv/main.py
@@ -1,5 +1,5 @@
 '''
-code required lot of cleaning. Use csv library instead
+code required a lot of cleaning. Use csv library instead
 with open("weather_data.csv", 'r') as data_file:
     content = data_file.readlines()
     print(type(content[1]))
@@ -18,7 +18,7 @@ with open("weather_data.csv", 'r') as data_file:
     print(temperatures)
 """
 
-import pandas
+"""import pandas
 
 data = pandas.read_csv("weather_data.csv")
 data_dic = data.to_dict()
@@ -37,9 +37,26 @@ print(type(data.columns))
 
 # to print row where the temp was at the max
 print(data[data.temp == max(data_list)])
+"""
+"""
+To create csv file with two columns Fur color and Count
+and count all squirels of each color in 2018 central park document 
+"""
 
+import pandas as pd
+file_from = "2018_Central_Park_Squirrel_Census_-_Squirrel_Data_20240416.csv"
+file_to = "squirel_count.csv"
+data = pd.read_csv(file_from)
+grey_color = len(data[data['Primary Fur Color'] == 'Gray'])
+cinnamon_color = len(data[ data['Primary Fur Color'] == 'Cinnamon'])
+black_color = len(data[data['Primary Fur Color'] == 'Black'])
 
-
+count_by_color = {
+    'Primary Fur Color' : ['Gray', 'Cinnamon', 'Black'],
+    'Count' : [grey_color, black_color, cinnamon_color]
+}
+data_to_csv = pd.DataFrame(count_by_color)
+data_to_csv.to_csv(file_to, index=False, lineterminator=',\n')
 
 
 
